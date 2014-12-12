2c6a84031be7b19d2415b5cab28800746e190c66
Samuel Just <sjust@redhat.com>
2014-12-12 14:06:27 -0800

    osd/: move recovery into opwq
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -9406,7 +9406,7 @@ void ReplicatedPG::mark_all_unfound_lost(int what)
   share_pg_log();
 
   // queue ourselves so that we push the (now-lost) object_infos to replicas.
-  osd->queue_for_recovery(this);
+  queue_recovery();
 }
 
 void ReplicatedPG::_finish_mark_all_unfound_lost(list<ObjectContextRef>& obcs)
@@ -9538,7 +9538,6 @@ void ReplicatedPG::on_shutdown()
   dout(10) << "on_shutdown" << dendl;
 
   // remove from queues
-  osd->recovery_wq.dequeue(this);
   osd->pg_stat_queue_dequeue(this);
   osd->dequeue_pg(this, 0);
   osd->peering_wq.dequeue(this);
